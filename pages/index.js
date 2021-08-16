29d16cea1640ca830134ebc2d2d9ca6878db7c84
PeteChu <phuengton.ch@gmail.com>
2021-08-17 00:12:23 +0700

    added introduction in mobile screen size

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,19 @@ export default function Home() {
       <Head>
         <title>PeteChu</title>
       </Head>
-      <section className="h-screen flex flex-col justify-center items-center">
-        <div className="introduce font-source-code-pro">
-          <div className="hidden md:block">
-            <p className="introduce-name my-1">Hello, my name is 
-              <span className="alias-name"> Phuengton.</span>
-            </p>
+      <section className="h-screen flex flex-col justify-center items-center font-source-code-pro">
+        <div className="introduce hidden md:block">
+          <p className="introduce-name my-1">
+            Hello, my name is
+            <span className="alias-name"> Phuengton.</span>
+          </p>
+        </div>
+        <div className="flex flex-col">
+          <div className="introduce mobile-screen md:hidden">
+            <p className="introduce-mobile-hello my-1">Hello, my name is</p>
           </div>
-          <div className="mobile-screen md:hidden">
-            <p className="introduce-hello my-1">Hello, my name is </p>
-            <p className="introduce-name alias-name"> Phuengton.</p>
+          <div className="introduce mobile-screen md:hidden w-min self-center">
+            <p className="introduce-mobile-name alias-name">Phuengton.</p>
           </div>
         </div>
         {/* <p className="introduce-position pt-8 text-white">I'm a Full Stack Developer.</p> */}
