6bf5e4f8c050291a86def90c4c6af40c50ec6891
PeteChu <21036379+PeteChu@users.noreply.github.com>
2021-08-30 22:12:20 +0700

    update index translate duration (#9)
    Co-authored-by: PeteChu <phuengton.ch@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import type { AppProps } from "next/app";
 import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <NextNProgress />
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
