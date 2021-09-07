41807ee9c26e9eef99d56246e8cc6845aa8f871a
PeteChu <phuengton.ch@gmail.com>
2021-09-07 23:04:12 +0700

    added styled-components

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,22 +2,44 @@ import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
+import styled from "styled-components";
+
+const Card = styled.div.attrs({
+    className:
+        "h-fit-content w-3/4 md:w-2/3 my-4 bg-gray-100 rounded-xl cursor-pointer",
+})``;
+
+const Title = styled.p.attrs({
+    className:
+        "text-xl font-semibold hover:text-blue-800 hover:text-opacity-90",
+})``;
+
+const Tag = styled.a.attrs({
+    className:
+        "mx-1 text-gray-600 hover:text-black text-sm border border-blue-100 hover:border-blue-300 bg-blue-100 bg-opacity-50 rounded text-center m-0.5 p-1 cursor-pointer",
+})``;
+
+const ImageCover = styled.div.attrs({
+    className: "mt-4 h-44 md:h-60",
+})``;
+
 function Blog({ posts = [] }) {
     return (
         <div className="bg-black flex flex-wrap justify-around">
             <Head>
                 <title>Blog</title>
             </Head>
-            {posts.map((post, index) => {
+            {posts.slice(-10).map((post, index) => {
                 return (
-                    <div
-                        className="flex flex-col h-fit-content w-3/4 md:w-2/3 my-4 bg-gray-100 rounded-xl cursor-pointer"
+                    <Card
+                        className="flex flex-col"
                         key={post.id}
+                        onClick={() => {
+                            window.open(post.url, "_blank");
+                        }}
                     >
                         <div className="flex-grow my-2 pt-2 px-4">
-                            <p className="text-xl font-semibold hover:text-blue-800 hover:text-opacity-90">
-                                {post.title}
-                            </p>
+                            <Title>{post.title}</Title>
                             <small className="font-light">
                                 {post.published_at}
                             </small>
@@ -27,19 +49,21 @@ function Blog({ posts = [] }) {
                             <div className="mt-2 tags flex flex-wrap">
                                 {post.tag_list.map((tag) => {
                                     return (
-                                        <a
-                                            className="tag mx-1 text-gray-600 hover:text-black text-sm border border-blue-100 hover:border-blue-300 bg-blue-100 bg-opacity-50 rounded text-center m-0.5 p-1 cursor-pointer"
+                                        <Tag
                                             href="#"
                                             key={`${index}-${tag}`}
+                                            onClick={(e) => {
+                                                e.stopPropagation();
+                                            }}
                                         >
                                             {`#${tag}`}
-                                        </a>
+                                        </Tag>
                                     );
                                 })}
                             </div>
                         </div>
                         {post.cover_image && (
-                            <div className="relative mt-4 h-44 md:h-60">
+                            <ImageCover className="relative">
                                 <Image
                                     className="cover-image rounded-b-xl"
                                     src={post.cover_image}
@@ -47,9 +71,9 @@ function Blog({ posts = [] }) {
                                     objectFit="cover"
                                     alt="article image cover"
                                 ></Image>
-                            </div>
+                            </ImageCover>
                         )}
-                    </div>
+                    </Card>
                 );
             })}
         </div>
