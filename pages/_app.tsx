461949135d616eed8b22b850fa6a28066601197f
PeteChu <phuengton.ch@gmail.com>
2022-12-02 11:56:54 +0700

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.scss";
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Component {...pageProps} />
+            <Analytics />
         </>
     );
 }
